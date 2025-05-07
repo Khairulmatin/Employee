0978b8cefec63c6364aea182dc2278b08bc7c3c4
Khairulmatin <khairulmatin302@gmail.com>
2025-05-07 14:35:22 +0530

    changes made

--- Main.java
+++ Main.java
@@ -2,20 +2,24 @@ class Employee {
     String name;
     int id;
     double salary;
-    public Employee(String name,int id ,double salary){
+    String address;
+    public Employee(String name,int id ,double salary,String address){
         this.name=name;
         this.id=id;
         this.salary=salary;
+        this.address=address;
     }
 
     public void displayInfo(){
         System.out.println("employee id:"+id); 
         System.out.println("employee name:"+name);
         System.out.println("employee salary:"+salary);
+        System.out.println("employee address:"+address);
     }
+    
 
     public static void main(String[] args) {
-        Employee emp1= new Employee("xyz", 101, 50000.0);
+        Employee emp1= new Employee("xyz", 101, 50000.0,"Hubli");
         emp1.displayInfo();
     }
 }
